00ed9ecc5fdcbae28bf7f7f24e284ba357c61847
galina <gpischalo@belta.by>
2020-04-14 19:16:32 +0300

    move process

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
-const { PORT } = require('./common/config');
-const app = require('./app');
 const { logger } = require('./middleware/logger');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
 process
   .on('unhandledRejection', reason => {
     logger.error(`Unhandled Rejection at Promise: ${reason.message}`);
-    logger.on('finish', () => process.exit(1));
+    process.exitCode = 1;
   })
   .on('uncaughtException', error => {
     logger.error(`Uncaught Exception: ${error}`);
-    logger.on('finish', () => process.exit(1));
+    process.exitCode = 1;
   });
 
 // PUT IT HERE for Review
 // throw Error('Oops!');
 // Promise.reject(Error('Oops!'));
+
+const { PORT } = require('./common/config');
+const app = require('./app');
+
+app.listen(PORT, () =>
+  console.log(`App is running on http://localhost:${PORT}`)
+);
